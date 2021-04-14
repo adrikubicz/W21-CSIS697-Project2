17537a8acbd13067317eb7440b18b44de1f4143c
wesmith340 <35306192+wesmith340@users.noreply.github.com>
2021-04-14 10:38:24 -0400

    Updating the gitignore

--- models/constants.py
+++
@@ -1,6 +0,0 @@
-
-tomtom_base='https://api.tomtom.com/search/2/geocode/'
-tomtom_route='https://api.tomtom.com/routing/1/calculateRoute/'
-tomtom_key = 'INSERT YOUR KEY'
-weatherStack_base='http://api.weatherstack.com/current'
-weatherStack_key='INSERT YOUR KEY'
